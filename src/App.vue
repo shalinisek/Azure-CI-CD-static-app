b6905f62c2329fcd83932f87c2b3498e769585ad
sarath <03sarath@gmail.com>
2026-01-24 22:33:32 +0530

    Updated title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>Welcome to MLOps world AWS, Azure, GCP :) </h1>
+    <h1>Welcome to the world of AI </h1>
 	<h2>Upload your data to get Gen Ai predictions :) </h2>
     <img
       alt="Vue logo"
